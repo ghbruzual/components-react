25817759390fcc1540f4dd5c2dcbf5f20974116d
german <ghbruzual@gmail.com>
2026-03-27 21:56:26 -0500

    git commit -m "feat: add random increment functionality and simplify button handlers"

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+// import React from "react";
+import Counter from "./Counter";
+
+function App() {
+  return (
+    <div>
+      <h1>Components React</h1>
+      <p>by Ghbruzual - fullstack developer</p>
+      <hr />
+      <Counter />
+      <hr />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Counter.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+export default function Counter() {
+  let [counter, setCounter] = React.useState(0);
+
+  function handleCounter(number) {
+    if (number === "random") {
+      const randomValue = Math.floor(Math.random() * 41 - 20);
+      setCounter(prev => prev + randomValue);
+    } else {
+      setCounter(prev => prev + number);
+    }
+  }
+
+  return (
+    <div>
+      <section className="counter-section">
+        <h2>Counter Component</h2>
+        <h3>Valor actual:</h3>
+        <p>{counter}</p>
+        <section className="counter-buttons">
+          <button onClick={() => handleCounter(1)}>+</button>
+          <button onClick={() => handleCounter(10)}>++</button>
+          <button onClick={() => setCounter(0)}>Reset</button>
+          <button onClick={() => handleCounter("random")}>Random</button>
+          <button onClick={() => handleCounter(-10)}>--</button>
+          <button onClick={() => handleCounter(-1)}>-</button>
+        </section>
+      </section>
+    </div>
+  );
+}
