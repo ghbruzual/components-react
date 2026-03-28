2b2d048263d1a2202ca6c0de508dbf08cf6c49e4
german <ghbruzual@gmail.com>
2026-03-27 22:31:16 -0500

    feat: add some styles and basic html for presentation

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
-// import React from "react";
 import Counter from "./Counter";
 
 function App() {
   return (
-    <div>
-      <h1>Components React</h1>
-      <p>by Ghbruzual - fullstack developer</p>
+    <div className="container">
+      <div className="tittle">
+        <img src="/src/assets/ghbruzualSinFondo.jpg" alt="logo-ghbruzual" />
+        <div>
+          <h1>Components React</h1>
+          <p>by Ghbruzual - fullstack developer</p>
+        </div>
+      </div>
+
       <hr />
       <Counter />
       <hr />
